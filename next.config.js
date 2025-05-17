05df1b2387dab79d86f77e6b76f904219180b779
maroua <maroua.boudoukha@efrei.net>
2025-05-17 22:01:24 +0200

    Fix vercel.json and package.json configuration

--- next.config.js
+++
@@ -1,6 +0,0 @@
-// This file loads the TypeScript configuration
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const { default: config } = require('./next.config.js.mjs');
-
-module.exports = config;
